e37fb0faab2d0873d4e317fe2b62ae52d5959151
Sambhav Mehta <sambhavmehta361@gmail.com>
2024-05-17 01:36:07 +0530

    eleventh commit

--- server/app.js
+++ server/app.js
@@ -2,19 +2,23 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import InfoSchema from './models/info.js';
+
+const app = express();
+
 mongoose
   .connect(
     'mongodb+srv://sambhavmehta:foHSMm3ALKrfbxWR@cluster0.eztpe0l.mongodb.net/Information'
   )
   .then((result) => {
-    console.log('Connected!');
-    console.log('Hehe');
-    app.listen(3000);
+    console.log('Connected to MongoDB!');
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
   })
   .catch((err) => {
-    console.log(err);
+    console.error('Error connecting to MongoDB:', err);
   });
-const app = express();
+
 app.use(cors());
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
@@ -22,6 +26,7 @@ app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.get('/', (req, res) => {
   res.send("This is a VITB-INSIGHTS' API by Team 139");
 });
+
 app.post('/api/form-data', (req, res) => {
   const formData = req.body;
   console.log('Received form data:', formData);
@@ -31,7 +36,7 @@ app.post('/api/form-data', (req, res) => {
       res.status(200).send('Form data received successfully');
     })
     .catch((err) => {
-      console.log(err);
+      console.error('Error saving form data to database:', err);
+      res.status(500).send('Error saving form data to database');
     });
-  //   res.status(200).send('Form data received successfully');
 });
